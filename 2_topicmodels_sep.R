941392fd37987bc9da3323f54835ac538b40aa91
Christian Czymara <czymara@gmail.com>
2020-04-17 11:38:33 +0200

    update

--- 2_topicmodels_sep.R
+++ 2_topicmodels_sep.R
@@ -22,30 +22,96 @@ setwd(root.dir)
 
 load("coRona2/in/data.RData")
 
+Ntopic <- 8
+
+data <- data[ which(data$OF01_01 != ""), ]
+
+
+## very lonely
+datalononlon <- data[ which(data$longquart == "[11,22]" | data$longquart == "(29,55]") , ]
+
+# living alone
+datalononlon %<>%
+  mutate(londum = if_else(longquart == "[11,22]",
+                          "not lonely",
+                          if_else(longquart == "(29,55]",
+                                  "very lonely",
+                                  NULL)))
+
+
+corpus_londum <- corpus(as.character(datalononlon$OF01_01),
+                     docvars = data.frame(socmeduse = datalononlon$ME02_03,
+                                          lialone = datalononlon$lialone,
+                                          londum = datalononlon$londum
+                     ))
+
+toks_londum <- tokens(corpus_londum, remove_punct = T,
+                   remove_numbers = T,
+                   remove_symbols = T,
+                   remove_separators = F,
+                   split_hyphens = T,
+                   remove_url = T,
+                   include_docvars = T)
+
+stopWords_de <- read.table("coRona2/in/stopwords-de.txt", encoding = "UTF-8", colClasses=c("character"))
+
+toks_londum <-  tokens_remove(toks_londum, c(stopWords_de$V1, stopwords("german")),
+                           case_insensitive = TRUE, padding = FALSE)
+
+toks_londum <- tokens_wordstem(toks_londum, language = "german")
+
+DFM_londum <- dfm(toks_londum)
+
+DFM_londum <- dfm_trim(DFM_londum, max_docfreq = 0.20,  min_docfreq = 0.001, docfreq_type = "prop")
+
+
+rowsum_londum <- apply(DFM_londum , 1, sum) # identify text with no common terms
+DFM_londum   <- DFM_londum[rowsum_londum > 0, ]  #remove all docs without these terms
+
+DFM_londum <- DFM_londum[complete.cases(DFM_londum@docvars$londum), ] # listwise deletion
+
+
+# model
+lonelydum_topics <- stm(DFM_londum,
+                     K = Ntopic,
+                     seed = 1337,
+                     verbose = T,
+                     init.type = "Spectral",
+                     prevalence = ~londum,
+                     content = ~londum,
+                     data = DFM_londum@docvars
+                     #, control = list(alpha = 1)
+                     )
+
+summary(lonelydum_topics)
+
+sapply(1:8, function(x){
+  plot(lonelydum_topics, type = "perspectives", topics = x)
+  dev.copy(png,paste("coRona2/out/private_topic_loncomp_", x, ".png", sep=""))
+  dev.off()
+  })
+
+
 
 ## very lonely
 datalon <- data[ which(data$longquart == "(29,55]"), ]
 
-datalon <- datalon[ which(datalon$OF01_01 != ""), ]
-
 
 corpus_lon <- corpus(as.character(datalon$OF01_01),
-                      docvars = data.frame(socmeduse = datalon$ME02_03,
-                                           lialone = datalon$lialone
-                      ))
+                     docvars = data.frame(socmeduse = datalon$ME02_03,
+                                          lialone = datalon$lialone
+                     ))
 
 toks_lon <- tokens(corpus_lon, remove_punct = T,
-                    remove_numbers = T,
-                    remove_symbols = T,
-                    remove_separators = F,
-                    split_hyphens = T,
-                    remove_url = T,
-                    include_docvars = T)
-
-stopWords_de <- read.table("coRona2/in/stopwords-de.txt", encoding = "UTF-8", colClasses=c("character"))
+                   remove_numbers = T,
+                   remove_symbols = T,
+                   remove_separators = F,
+                   split_hyphens = T,
+                   remove_url = T,
+                   include_docvars = T)
 
 toks_lon <-  tokens_remove(toks_lon, c(stopWords_de$V1, stopwords("german")),
-                            case_insensitive = TRUE, padding = FALSE)
+                           case_insensitive = TRUE, padding = FALSE)
 
 toks_lon <- tokens_wordstem(toks_lon, language = "german")
 
@@ -62,14 +128,14 @@ DFM_lon <- DFM_lon[complete.cases(DFM_lon@docvars$lialone), ] # listwise deletio
 
 # model
 lonely_topics <- stm(DFM_lon,
-                      K = Ntopic,
-                      seed = 1337,
-                      verbose = T,
-                      init.type = "Spectral",
-                      prevalence =~lialone,
-                      data = DFM_lon@docvars
-                      #, control = list(alpha = 1)
-                     )
+                     K = Ntopic,
+                     seed = 1337,
+                     verbose = T,
+                     init.type = "Spectral",
+                   #  prevalence =~lialone,
+                     data = DFM_lon@docvars
+                     #, control = list(alpha = 1)
+                   )
 
 topic_lon_privat <- summary(lonely_topics)
 topic_lon_privat <- topic_lon_privat$prob
@@ -82,11 +148,8 @@ dev.copy(png,"coRona2/out/private_topic_lonely_freq.png")
 dev.off()
 
 
-
 ## not lonely
 datanotlon <- data[ which(data$longquart == "[11,22]"), ]
-
-datanotlon <- datanotlon[ which(datanotlon$OF01_01 != ""), ]
 
 
 corpus_notlon <- corpus(as.character(datanotlon$OF01_01),
@@ -124,7 +187,7 @@ notlonely_topics <- stm(DFM_notlon,
                      seed = 1337,
                      verbose = T,
                      init.type = "Spectral",
-                     prevalence =~lialone,
+                    # prevalence =~lialone,
                      data = DFM_notlon@docvars
                      #, control = list(alpha = 1)
                      )
